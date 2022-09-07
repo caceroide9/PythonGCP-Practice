d54828c944cbf60a0fb0812d223b1e3577fac357
caceroide9 <mathias.caceresb@utem.cl>
2022-09-07 16:52:08 -0400

    CloudRunDeploy

--- CloudRun/Flask_ETL/src/app.py
+++ CloudRun/Flask_ETL/src/app.py
@@ -1,17 +1,83 @@
-from flask import Flask,jsonify,render_template
-
+from flask import Flask,jsonify,render_template,request
 from config import config
-from flask_mysqldb import MySQL
+from db import *
+
 
 
 STATIC_FOLDER = 'templates/assets'
 app = Flask(__name__,static_folder=STATIC_FOLDER)
-conexion= MySQL(app)
 
-@app.route('/',methods=['GET'])
-def ping():
-    return render_template("index.html")
+@app.route('/', methods=['GET'])
+def get_songs():
+    return get()
+
+
+@app.route('/add', methods=['POST'])
+def add_song():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    create(request.get_json())
+    return 'Song Added'
+
+@app.route('/index')
+def index():
+    return render_template('layout.html')
+
+@app.route('/complete')
+def aux():
+    return render_template('index.html', contacts=get2())
+
+
+@app.route('/music', methods=['GET'])
+def listar_cursos():
+    return listmusic()
+        
+@app.route('/music/<codigo>', methods=['GET'])
+def leer_curso(codigo):
+    try:
+        curso = leer_cancion(codigo)
+        if curso != None:
+            return curso
+        else:
+            return jsonify({'mensaje': "Curso no encontrado.", 'exito': False})
+    except Exception as ex:
+        return jsonify({'mensaje': "Error", 'exito': False})
+
+@app.route('/music', methods=['POST'])
+def registrar_curso():
+    # print(request.json)
+    try:
+        curso = leer_cancion(request.json['codigo'])
+        if curso != None:
+            return jsonify({'mensaje': "Código ya existe, no se puede duplicar.", 'exito': False})
+        else:
+            post_cancion(request)
+    except Exception as ex:
+        return jsonify({'mensaje': "Error", 'exito': False})
+        
+@app.route('/cursos/<codigo>', methods=['PUT'])
+def actualizar_curso(codigo):
+    try:
+        curso = leer_cancion(codigo)
+        if curso != None:
+            put_cancion(request,codigo)
+        else:
+            return jsonify({'mensaje': "Curso no encontrado.", 'exito': False})
+    except Exception as ex:
+        return jsonify({'mensaje': "Error", 'exito': False})
+
+@app.route('/cursos/<codigo>', methods=['DELETE'])
+def eliminar_curso(codigo):
+    try:
+        curso = leer_cancion(codigo)
+        if curso != None:
+            eliminar_cancion(codigo)
+        else:
+            return jsonify({'mensaje': "Curso no encontrado.", 'exito': False})
+    except Exception as ex:
+        return jsonify({'mensaje': "Error", 'exito': False})
 
 if __name__ == "__main__":
     app.config.from_object(config['development'])
-    app.run(host='0.0.0.0', port=8080,debug=True) 
+    app.run(host='0.0.0.0', port=8080,debug=True)
---
+++ CloudRun/Flask_ETL/src/db.py
@@ -0,0 +1,107 @@
+import os
+import pymysql
+from flask import jsonify
+
+#db_user = os.environ.get('CLOUD_SQL_USERNAME')
+#db_password = os.environ.get('CLOUD_SQL_PASSWORD')
+#db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
+#db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+
+
+def open_connection():
+    conn = pymysql.connect(user=db_user,password=db_password,unix_socket=db_connection_name,db=db_name,cursorclass=pymysql.cursors.DictCursor)
+    return conn
+
+
+def get():
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        result = cursor.execute('SELECT * FROM songs;')
+        songs = cursor.fetchall()
+        if result > 0:
+            got_songs = jsonify(songs)
+        else:
+            got_songs = 'No Songs in DB'
+        return got_songs
+
+
+def create(song):
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        cursor.execute('INSERT INTO songs (title, artist, genre) VALUES(%s, %s, %s)',
+                       (song["title"], song["artist"], song["genre"]))
+    conn.commit()
+    conn.close()
+
+
+def get2():
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        result = cursor.execute('SELECT * FROM songs;')
+        songs = cursor.fetchall()
+        if result > 0:
+            got_songs = (songs)
+        else:
+            got_songs = 'No Songs in DB'
+        return got_songs
+
+
+def listmusic():
+    try:
+        conn = open_connection()
+        with conn.cursor() as cursor:
+            sql = "SELECT title, artist, genre FROM songs ORDER BY nombre ASC"
+            cursor.execute(sql)
+            datos = cursor.fetchall()
+            cursos = []
+            for fila in datos:
+                curso = {'title': fila[0], 'artist': fila[1], 'genre': fila[2]}
+                cursos.append(curso)
+        return cursos
+    except Exception as ex:
+        return ({'mensaje': "Error", 'exito': False})
+
+
+def leer_cancion(codigo):
+    try:
+        conn = open_connection()
+        with conn.cursor() as cursor:
+            sql = "SELECT title, artist, genre FROM songs WHERE title = '{0}'".format(codigo)
+            cursor.execute(sql)
+            datos = cursor.fetchone()
+            if datos != None:
+                curso = {'title': datos[0], 'artist': datos[1], 'genre': datos[2]}
+                return curso
+            else:
+                return None
+        
+    except Exception as ex:
+        raise ex
+
+
+def post_cancion(request):
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        sql = """INSERT INTO songs (title, artist, genre) 
+             VALUES ('{0}', '{1}', {2})""".format(request.json['title'],
+                                                     request.json['artist'], request.json['genre'])
+    cursor.execute(sql)
+    conn.connection.commit()  # Confirma la acción de inserción.
+    return jsonify({'mensaje': "Curso registrado.", 'exito': True})
+
+def put_cancion(request,codigo):
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        sql = """UPDATE curso SET artist = '{0}', genre = {1} 
+                WHERE title = '{2}'""".format(request.json['artist'], request.json['genre'], codigo)
+        cursor.execute(sql)
+        conn.connection.commit()  # Confirma la acción de actualización.
+        return jsonify({'mensaje': "Curso actualizado.", 'exito': True})
+
+def eliminar_cancion(codigo):
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        sql = "DELETE FROM curso WHERE codigo = '{0}'".format(codigo)
+        cursor.execute(sql)
+        conn.connection.commit()  # Confirma la acción de eliminación.
+        return jsonify({'mensaje': "Curso eliminado.", 'exito': True})
